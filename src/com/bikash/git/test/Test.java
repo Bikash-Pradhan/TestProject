5730dc4107915f14cc2a801dc8815c4c4176f912
Bikash-Pradhan <bikashrp88@gmail.com>
2017-08-13 19:55:36 +0530

    Slack integration testing.

--- src/com/bikash/git/test/Test.java
+++ src/com/bikash/git/test/Test.java
@@ -3,7 +3,7 @@ package com.bikash.git.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("My 1st git project.");
+		System.out.println("My 1st git project. Testing Slack integration.");
 	}
 
 }
